314502445b048162cc112f2b746f0f03afa4fc4a
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2012-10-11 22:53:37 +0200

    ścieżki dla gałezi produkcja

--- libgkonap/gkpath.py
+++ libgkonap/gkpath.py
@@ -1,4 +1,4 @@
-domyslny_katalog = "$HOME/git/gkonap-old/test/"
-config_file = "$HOME/git/gkonap-old/test/config/config_file"
-icon = "/home/daniel/git/gkonap-old/libgkonap/gfx/icon.svg"
-wersja = 'dev'
+domyslny_katalog = "$HOME"
+config_file = "$HOME/.config/gkonap.cfg"
+icon = "/usr/share/pixmaps/gkonap/icon.svg"
+wersja = '0.5.2'
